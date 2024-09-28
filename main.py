04848bcec31cba79d4ad133e95b3f44281bb88f9
Kiran Aryal <kiranaryal1997@gmail.com>
2024-09-28 20:07:43 +0545

    Implementing the generate Method.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fpdf import FPDF
 class Bill:
     """
     Object that contains data about a bill such as total amount
@@ -34,14 +35,37 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+        pdf = FPDF(orientation="P", unit="pt", format="A4")
+        pdf.add_page()
 
-my_bill = Bill(amount=120, period="September, 2024")
+        #Adding the title
+        pdf.set_font(family="Times", size=24, style="B")
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
+
+        #Insert period label and the value
+        pdf.cell(w=150, h=40, txt="Period:", border=1)
+        pdf.cell(w=0, h=40, txt=bill.period, border=1, ln=1)
+
+        # Insert name and due amount of the first flatmate
+        pdf.cell(w=150, h=40, txt=flatmate1.name, border=1)
+        pdf.cell(w=0, h=40, txt=str(flatmate1.pays(bill, flatmate2)), border=1, ln=1)
+
+        # # Insert name and due amount of the second flatmate
+        pdf.cell(w=150, h=40, txt=flatmate2.name, border=1)
+        pdf.cell(w=0, h=40, txt=str(flatmate2.pays(bill, flatmate1)), border=1)
+
+        pdf.output(self.filename)
+
+
+my_bill = Bill(amount=120, period="May, 2024")
 pawan = Flatmate(name="Pawan", days_in_house=20)
 kiran = Flatmate(name="Kiran", days_in_house=25)
 
 print(f"{pawan.name} pays:", pawan.pays(bill=my_bill, flatmate2=kiran))
 print(f"{kiran.name} pays:", kiran.pays(bill=my_bill, flatmate2=pawan))
+
+pdf_report = PdfReport(filename="Report1.pdf")
+pdf_report.generate(flatmate1=pawan, flatmate2=kiran, bill=my_bill)
 
 
 
