e176070b783548c44bba51149b676ef5d74d9294
Kiran Aryal <kiranaryal1997@gmail.com>
2024-09-28 22:17:14 +0545

    Changing PDF Text font.

--- main.py
+++ main.py
@@ -46,19 +46,21 @@ class PdfReport:
 
         #Adding the title
         pdf.set_font(family="Times", size=24, style="B")
-        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
 
+        pdf.set_font(family="Times", size=14, style="B")
         #Insert period label and the value
-        pdf.cell(w=150, h=40, txt="Period:", border=1)
-        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=100, h=40, txt=bill.period, border=0, ln=1)
 
+        pdf.set_font(family="Times", size=14)
         # Insert name and due amount of the first flatmate
-        pdf.cell(w=150, h=40, txt=flatmate1.name, border=1)
-        pdf.cell(w=150, h=40, txt=flatmate1_pay, border=1, ln=1)
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=0)
+        pdf.cell(w=100, h=40, txt=flatmate1_pay, border=0, ln=1)
 
         # # Insert name and due amount of the second flatmate
-        pdf.cell(w=150, h=40, txt=flatmate2.name, border=1)
-        pdf.cell(w=150, h=40, txt=flatmate2_pay, border=1)
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=0)
+        pdf.cell(w=100, h=40, txt=flatmate2_pay, border=0)
 
         pdf.output(self.filename)
 
