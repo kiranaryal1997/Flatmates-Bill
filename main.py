78932b93bc26ebcd090bfe99cc15c0477cc9aba6
Kiran Aryal <kiranaryal1997@gmail.com>
2024-09-28 15:56:41 +0545

    Creating an empty class files based on the design.

---
+++ main.py
@@ -0,0 +1,42 @@
+class Bill:
+    """
+    Object that contains data about a bill such as total amount
+    and period of the bill.
+    """
+
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
+
+
+class Flatmate:
+    """
+    Create a flatmate person who lives in the flat and pays a share of a bill.
+    """
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        return bill.amount/2
+
+
+class PdfReport:
+    """
+    Creates a pdf file that contains data about the flatmates such
+    as their names, their due amount and the period of the bill.
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
+
+
+
+
+
+
+
